bb4d451bf625fa2a0da218f2a88c68046ab2a455
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-04-14 18:23:03 +0200

    update test_package

--- test_package/test_package.c
+++ test_package/test_package.c
@@ -5,15 +5,17 @@ int main() {
   appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
   appInfo.pApplicationName = "Hello Conan";
   appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-  appInfo.pEngineName = "No Engine";
+  appInfo.pEngineName = "Dummy Engine";
   appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
   appInfo.apiVersion = VK_API_VERSION_1_0;
 
   VkInstanceCreateInfo createInfo;
   createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
   createInfo.pApplicationInfo = &appInfo;
-  createInfo.enabledExtensionCount = 0;
   createInfo.enabledLayerCount = 0;
+  createInfo.ppEnabledLayerNames = NULL;
+  createInfo.enabledExtensionCount = 0;
+  createInfo.ppEnabledExtensionNames = NULL;
 
   return 0;
 }
